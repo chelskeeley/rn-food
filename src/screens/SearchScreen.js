a7249c5f5994cfad65e0b254c547158086138584
chelskeeley <chelsea.keeley@gmail.com>
2020-10-16 11:36:34 -0400

    Add search endpoint

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,34 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose"
+      }
+    });
+
+    setResults(response.data.businesses)
+  }
 
   return (
     <View style={styles.background}>
       <SearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log("Term was submitted")}
+        onTermSubmit={searchApi}
       />
       <Text>Search Screen</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 }
